1469587516e556553628fc7797907e41a3486780
Abhijeet Sasmal <abhijeet.work.1512@gmail.com>
2023-11-14 02:14:14 +0530

    worked on hydra

--- src/process.py
+++ src/process.py
@@ -4,17 +4,34 @@ This is the demo code that uses hydra to access the parameters in under the dire
 Author: Khuyen Tran
 """
 
-import hydra
+import tensorflow as tf
 from omegaconf import DictConfig
 
 
-@hydra.main(config_path="../config", config_name="main", version_base=None)
 def process_data(config: DictConfig):
     """Function to process the data"""
+    train_ds = tf.keras.utils.image_dataset_from_directory(
+        config.data_train.raw_train,
+        validation_split=0.2,
+        subset="training",
+        seed=config.process.seed,
+        image_size=(config.process.img_height, config.process.img_width),
+        batch_size=config.process.batch_size,
+    )
 
-    print(f"Process data using {config.data.raw}")
-    print(f"Columns used: {config.process.use_columns}")
+    val_ds = tf.keras.utils.image_dataset_from_directory(
+        config.data_train.raw_train,
+        validation_split=0.2,
+        subset="validation",
+        seed=config.process.seed,
+        image_size=(config.process.img_height, config.process.img_width),
+        batch_size=config.process.batch_size,
+    )
 
+    class_names = train_ds.class_names
 
-if __name__ == "__main__":
-    process_data()
+    AUTOTUNE = tf.data.AUTOTUNE
+    train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+    return train_ds, val_ds, class_names
--- src/train_model.py
+++ src/train_model.py
@@ -5,16 +5,44 @@ Author: Khuyen Tran
 """
 
 import hydra
+import tensorflow as tf
 from omegaconf import DictConfig
+from process import process_data
 
 
 @hydra.main(config_path="../config", config_name="main", version_base=None)
 def train_model(config: DictConfig):
     """Function to train the model"""
 
-    print(f"Train modeling using {config.data.processed}")
-    print(f"Model used: {config.model.name}")
-    print(f"Save the output to {config.data.final}")
+    train_ds, val_ds, class_names = process_data(config)
+
+    num_classes = len(class_names)
+
+    model = tf.keras.Sequential(
+        [
+            tf.keras.layers.Rescaling(1.0 / 255),
+            tf.keras.layers.Conv2D(32, 3, activation="relu"),
+            tf.keras.layers.MaxPooling2D(),
+            tf.keras.layers.Conv2D(32, 3, activation="relu"),
+            tf.keras.layers.MaxPooling2D(),
+            tf.keras.layers.Conv2D(32, 3, activation="relu"),
+            tf.keras.layers.MaxPooling2D(),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(128, activation="relu"),
+            tf.keras.layers.Dense(num_classes),
+        ]
+    )
+
+    model.compile(
+        optimizer="adam",
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=["accuracy"],
+    )
+
+    model.fit(train_ds, validation_data=val_ds, epochs=3)
+    model.save("../models/" + config.model.name)
+
+    print(f"Save the output to {config.model.name}")
 
 
 if __name__ == "__main__":
